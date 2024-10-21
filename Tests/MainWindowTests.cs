f68c74a1ba5abdbc6f83eb3aed92e65d83073721
Maks Rawski <maksymilian.rawski@tutanota.com>
2024-10-21 11:20:54 +0200

    add tests folder

---
+++ Tests/MainWindowTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ISA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ISA.Tests
+{
+    [TestClass()]
+    public class MainWindowTests
+    {
+        [DataTestMethod]
+        [DataRow(new double[] { 0.1, 0.3, 0.6, 1.0 }, 0.2, 1)]
+        [DataRow(new double[] { 0.1, 0.3, 0.6, 1.0 }, 0.5, 2)]
+        [DataRow(new double[] { 0.1, 0.3, 0.6, 1.0 }, 0.8, 3)]
+        [DataRow(new double[] { 0.1, 0.3, 0.6, 1.0 }, 0.1, 0)]
+        [DataRow(new double[] { 0.1, 0.3, 0.6, 1.0 }, 0.3, 1)]
+        [DataRow(new double[] { 0.1, 0.3, 0.6, 1.0 }, 0.6, 2)]
+        [DataRow(new double[] { 0.1, 0.3, 0.6, 1.0 }, 1.0, 3)]
+        public void GetCDFIndexTest(double[] qsArray, double r, int expectedIndex)
+        {
+            List<double> qs = new List<double>(qsArray);
+            int result = MainWindow.GetCDFIndex(r, qs);
+
+            // Assert
+            Assert.AreEqual(
+                expectedIndex,
+                result,
+                $"Test failed for r={r}, qs=[{string.Join(", ", qsArray)}]. Expected index: {expectedIndex}, but got: {result}."
+            );
+        }
+    }
+}
